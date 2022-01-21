b09921ea1c5397489f8ed9744041ac52c0ef2c9c
Rohit Singh <rohiisingh865@gmail.com>
2022-01-22 02:33:53 +0530

    initial commit

---
+++ Findnumberofsetbits.cpp
@@ -0,0 +1,27 @@
+// Given a positive integer N, print count of set bits in it.
+#include <iostream>
+#include <bits.h>
+class Solution
+{
+public:
+    int setbits(int N)
+    {
+        int count = 0;
+        while (N)
+        {
+            N = N & (N - 1);
+            count++;
+        }
+        return count;
+    }
+};
+using namespace std;
+int main()
+{
+    int N;
+    cin >> N;
+    Solution ob;
+    int cnt = ob.setbits(N);
+    cout << cnt << endl;
+    return 0;
+}
