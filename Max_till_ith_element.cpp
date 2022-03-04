ea30094b2d329a5a91ff4d09fd4d5fd31b780008
Rohit Singh <81810848+RohitSingh-N@users.noreply.github.com>
2022-03-04 18:25:40 +0530

    initial commit

---
+++ Max_till_ith_element.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int Arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> Arr[i];
+    }
+    int currmax = 0;
+    int totalmax = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (Arr[i] > Arr[i + 1])
+            currmax = Arr[i];
+        if (currmax > totalmax)
+            totalmax = currmax;
+    }
+    cout << "The maximum is --> " << totalmax;
+    return 0;
+}
+// 6
+// 1 5 8 6 3 4
