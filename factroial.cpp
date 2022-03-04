3d84bc16bb555c46add2f5f192fafcfec85d7c61
Rohit Singh <81810848+RohitSingh-N@users.noreply.github.com>
2022-03-04 17:36:53 +0530

    initial commit

---
+++ factroial.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int fact(int n)
+{
+    // base case
+    if (n == 1 || n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return (n * fact(n - 1));
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int factorial = fact(n);
+    cout << factorial;
+    return 0;
+}
+
+/*input-->5
+output-->120 */
