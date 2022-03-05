89b1f4075e299cadf07da9380e9d3f5589d2f14c
Rohit Singh <81810848+RohitSingh-N@users.noreply.github.com>
2022-03-06 01:43:25 +0530

    greater function

---
+++ maxnumber_from_a_String.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve(int n)
+{
+}
+int main()
+{
+    string str = "45464654";
+    /// greater function is used for sorting in desecending order
+    sort(str.begin(), str.end(), greater<char>());
+    cout << str << endl;
+    return 0;
+}
