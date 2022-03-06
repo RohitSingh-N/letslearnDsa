3859b7e0e6fdce00773e68c895600e63577f295d
Rohit Singh <81810848+RohitSingh-N@users.noreply.github.com>
2022-03-06 20:05:45 +0530

    bit Magic

--- numberofbitsflipped.cpp
+++ numberofbitsflipped.cpp
@@ -14,7 +14,7 @@ public:
         }
         return count;
     }
-    // Function to find number of bits needed to be flipped to convert A to B
+    // Function to find number of bits needed to be flipped to convert A to B.
     int countBitsFlip(int a, int b)
     {
         return countsetbits(a ^ b);
