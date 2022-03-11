70c520c51470762b1c94e61dd2bdecd9f44d705b
Rohit Singh <81810848+RohitSingh-N@users.noreply.github.com>
2022-03-11 13:30:59 +0530

    B tree

---
+++ build_from_level_order.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+void buildfromlevelorder(node *&root)
+{
+    cout << "enter data : "
+         << " ";
+    int data;
+    cin >> data;
+    root = new node(data);
+    queue<node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+
+        cout << "Enter data for left of : " << temp->data << endl;
+        int leftdata;
+        cin >> leftdata;
+        if (leftdata != -1)
+        {
+            temp->left = new node(leftdata);
+            q.push(temp->left);
+        }
+        cout << "Enter data for right of : " << temp->data << endl;
+        int rightdata;
+        cin >> rightdata;
+        if (rightdata != -1)
+        {
+            temp->right = new node(rightdata);
+            q.push(temp->right);
+        }
+    }
+}
+void level_order_trav(node *root)
+{
+    queue<node *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+        if (temp == NULL)
+        {
+            cout << endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout << temp->data << " ";
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+int main()
+{
+    /* 1 2 3 4 5 6 7 -1 -1 -1 -1 -1 -1 -1 -1 */
+    node *root = NULL;
+    buildfromlevelorder(root);
+    cout << "printing the B Tree: " << endl;
+    level_order_trav(root);
+    return 0;
+}
