58185b18ec344f2704ace7b33d79d162f8b63597
Rohit Singh <81810848+RohitSingh-N@users.noreply.github.com>
2022-03-12 19:21:43 +0530

    BST

---
+++ search_in_BST.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int da)
+    {
+        this->data = da;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+node *insertinbst(node *root, int d)
+{
+    if (root == NULL)
+    {
+        root = new node(d);
+        return root;
+    }
+    if (d > root->data)
+    {
+        root->right = insertinbst(root->right, d);
+    }
+    else
+    {
+        root->left = insertinbst(root->left, d);
+    }
+    return root;
+}
+void level_order_traversal(node *root)
+{
+    queue<node *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+
+        if (temp == NULL)
+        {
+            cout << endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout << temp->data << " ";
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+void binarysearch_tree(node *&root)
+{
+    int data;
+    cin >> data;
+
+    while (data != -1)
+    {
+        root = insertinbst(root, data);
+        cin >> data;
+    }
+}
+bool searchinbst(node *root, int k)
+{
+    if (root == NULL)
+    {
+        return false;
+    }
+    if (root->data == k)
+    {
+        return true;
+    }
+    if (root->data > k)
+    {
+        return searchinbst(root->left, k);
+    }
+    else
+    {
+        return searchinbst(root->right, k);
+    }
+}
+int main()
+{
+    /* 10 8 21 7 27 5 4 3 -1 */
+    node *root = NULL;
+    cout << "enter data to insert in bst: " << endl;
+    binarysearch_tree(root);
+    cout << "printing the bST: " << endl;
+    level_order_traversal(root);
+    bool ans = searchinbst(root, 99);
+    if (ans)
+    {
+        cout << "element is present!"
+             << " ";
+    }
+    else
+    {
+        cout << "element is not present!"
+             << " ";
+    }
+
+    return 0;
+}
